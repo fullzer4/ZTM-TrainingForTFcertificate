e832c6af6a55dc68d8f20b9e6e84a8f696b5ee89
fullzer4 <gabrielpelizzaro@gmail.com>
2023-03-06 09:46:48 -0300

    new

---
+++ C/pr2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (void) {
+
+    int x = 2;
+    int y = 4;
+
+    if (x < y){
+        printf("x is less than y");
+    }else if (x > y){
+        printf("x is more than y");
+    }else {
+        printf("x is equal than y");
+    }
+}
